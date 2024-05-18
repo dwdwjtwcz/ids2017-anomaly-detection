f7763970000ab0d2872110f308e6527ec3267e6f
Jakub Pomorski <delveller1@gmail.com>
2024-05-18 13:53:58 +0200

    folder rename

--- test.py
+++ test.py
@@ -9,4 +9,9 @@ import jax.numpy as jnp
 import functools as ft
 
 data = pd.read_parquet('./datasets/Benign-Monday-no-metadata.parquet')
-print(data.head())
+
+sns.set_style('whitegrid')
+plt.figure(figsize=(10, 6))
+sns.histplot(data['Flow Duration'], bins=50, kde=True)
+plt.title('Flow Duration Distribution')
+plt.show()
